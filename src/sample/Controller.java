217ca7457cb5fa457e4d0c29a1cbc806d4f3c0d7
wassimba1993 <wassimba1993@gmail.com>
2017-01-29 12:40:47 +0200

    +snackbar + view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSnackbar;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    JFXSnackbar snack;
+    @FXML
+    TextField text;
+    @FXML
+    JFXButton button;
+    @FXML
+    Pane pane;
+    static int nbaffichage = 0;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("ffff");
+            }
+        });
+        snack.registerSnackbarContainer(pane);
+        snack.refreshPopup();
+        button.setOnAction(e -> {
+            int delay = 10000;
+            String toshow = "";
+            String myString = "This text will be copied into clipboard when running this code!";
+            StringSelection stringSelection = new StringSelection(myString);
+            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clpbrd.setContents(stringSelection, null);
+            if (nbaffichage == 0) {
+                toshow = "Text copied into clipboard Press (CTRL+V) or (RightClick+Paste) to get it\n";
+                nbaffichage = 1;
+            } else {
+                delay = 5000;
+            }
+            toshow = toshow + "" + myString;
+
+            nbaffichage++;
+            snack.show(toshow, 6000);
+        });
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.security.*;
+
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 800, 507);
+        scene.getStylesheets().add("sample/style/style.css");
+        //
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("SHA1 Encrypt");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
